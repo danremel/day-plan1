06fe57bbaa2bbc6a72365af1d1b5305b2d25daf9
danremel <remel.dan@gmail.com>
2017-09-15 11:59:16 -0400

    Styled task cards

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const TaskCardStyles = styled.div`
+box-shadow: 1px 1px 1px black;
+border: 1px solid black;
+padding: 10px;
+width: 300px;
+height: auto;
+text-align: center;
+margin: 1.5% 0;
+background-color: #e1b97e;
+&:hover {
+  background-color: #E5C8A0;
+}
+`;
 
 const TaskCard = (props) => {
   const task = props.task;
   const day = props.day;
 
   return (
-    <div>
+    <TaskCardStyles>
       <Link to={`/days/${day.id}/tasks/${task.id}`}>
         <h3>Task: {task.name}</h3>
+        <hr/>
         <p>Description: {task.description}</p>
         <p>Priority level: {task.priority_level} out of 3</p>
         <p>To be completed by: {task.completion_time}</p>
       </Link>
-    </div>
+    </TaskCardStyles>
   );
 };
 
